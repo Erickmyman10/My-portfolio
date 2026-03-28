f2fa1b4515192914b3230861c1a91e51b4d8f3d9
Gabriel <gabrieladebayo10@gmail.com>
2026-03-28 17:57:09 +0100

    Initial portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Syne", "sans-serif"],
+        body: ["Manrope", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
